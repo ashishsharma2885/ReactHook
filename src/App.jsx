1fbc6f63458c1f888489d7fd71a935bee36fecdb
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2024-03-17 10:29:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+function App() {
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <div>
+      <form action='' onSubmit={handleSubmit(data => console.log(data))}>
+        <input {...register('name')} type='text' placeholder='name' />
+        <input {...register('email')} type='email' placeholder='email' />
+        <input type='submit' />
+      </form>
+    </div>
+  );
+}
+
+export default App;
